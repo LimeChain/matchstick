3326f2aff663547aaf5d1524728a4ca4b9936598
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-01-17 16:43:50 +0200

    Refactor test suite logic to map describes with functions and hooks

--- src/test_suite.rs
+++ src/test_suite.rs
@@ -2,12 +2,20 @@ use colored::Colorize;
 use graph::blockchain::Blockchain;
 use std::time::Instant;
 use wasmtime::Func;
+use twiggy_parser;
+use twiggy_analyze;
+use twiggy_opt;
+use twiggy_traits;
+use serde_json::{Value};
+use std::collections::BTreeMap;
+use regex::Regex;
 
 use crate::{
     instance::MatchstickInstance,
     logging::{self, Log},
 };
 
+#[derive(Debug)]
 pub struct Test {
     pub name: String,
     should_fail: bool,
@@ -114,10 +122,25 @@ impl Test {
     }
 }
 
-pub struct TestSuite {
+pub struct TestSuiteOld {
     pub tests: Vec<Test>,
     pub before_all: Vec<Func>,
     pub after_all: Vec<Func>,
+}
+
+#[derive(Debug)]
+pub struct TestSuite {
+    pub groups: BTreeMap<i32, TestGroup>,
+    pub before_all: Vec<Func>,
+    pub after_all: Vec<Func>,
+}
+
+#[derive(Debug)]
+pub struct TestGroup {
+    pub name: String,
+    pub before_all: Vec<Func>,
+    pub after_all: Vec<Func>,
+    pub tests: Vec<Test>
 }
 
 impl<C: Blockchain> From<&MatchstickInstance<C>> for TestSuite {
@@ -132,12 +155,32 @@ impl<C: Blockchain> From<&MatchstickInstance<C>> for TestSuite {
             );
         });
 
-        let mut suite = TestSuite {
+        let test_groups = test_groups(&matchstick.wasm);
+
+        let mut suite = TestSuiteOld {
             tests: vec![],
             before_all: vec![],
             after_all: vec![],
         };
 
+        let mut suite_new = TestSuite {
+            groups: BTreeMap::new(),
+            before_all: vec![],
+            after_all: vec![],
+        };
+
+        for (k, _v) in test_groups.iter() {
+            let test_group = TestGroup {
+                name: "".to_string(),
+                tests: vec![],
+                before_all: vec![],
+                after_all: vec![],
+            };
+            suite_new.groups.insert(*k, test_group);
+        }
+
+        let mut before_each_new: BTreeMap<i32, Vec<Func>> = BTreeMap::new();
+        let mut after_each_new: BTreeMap<i32, Vec<Func>> = BTreeMap::new();
         let mut before_each = vec![];
         let mut after_each = vec![];
         let mut tests = vec![];
@@ -170,14 +213,81 @@ impl<C: Blockchain> From<&MatchstickInstance<C>> for TestSuite {
                 .to_owned();
 
             match role.as_str() {
-                "beforeAll" => suite.before_all.push(func),
-                "afterAll" => suite.after_all.push(func),
-                "beforeEach" => before_each.push(func),
-                "afterEach" => after_each.push(func),
-                _ => tests.push(Test::new(name.to_string(), *should_fail, func)),
+                "beforeAll" => {
+                    let id = *func_idx as i32;
+                    let parent_id = get_parent_id(id.clone(), test_groups.clone());
+                    if parent_id == id {
+                        suite_new.before_all.push(func.clone());
+                    } else {
+                        suite_new.groups.get_mut(&parent_id).unwrap().before_all.push(func.clone());
+                    }
+
+                    suite.before_all.push(func.clone());
+                },
+                "afterAll" => {
+                    let id = *func_idx as i32;
+                    let parent_id = get_parent_id(id.clone(), test_groups.clone());
+                    if parent_id == id {
+                        suite_new.after_all.push(func.clone());
+                    } else {
+                        suite_new.groups.get_mut(&parent_id).unwrap().after_all.push(func.clone());
+                    }
+
+                    suite.after_all.push(func.clone());
+                },
+                "beforeEach" => {
+                    let id = *func_idx as i32;
+                    let parent_id = get_parent_id(id.clone(), test_groups.clone());
+                    if parent_id == id {
+                        for (_, group) in suite_new.groups.iter_mut() {
+                            group.before_all.push(func.clone());
+                        }
+                    } else {
+                        before_each_new.entry(parent_id).or_insert_with_key(|_| vec![func.clone()]);
+                    }
+
+                    before_each.push(func.clone())
+                },
+                "afterEach" => {
+                    let id = *func_idx as i32;
+                    let parent_id = get_parent_id(id.clone(), test_groups.clone());
+                    println!("{}", parent_id);
+                    if parent_id == id {
+                        for (_, group) in suite_new.groups.iter_mut() {
+                            group.after_all.push(func.clone());
+                        }
+                    } else {
+                        after_each_new.entry(parent_id).or_insert_with_key(|_| vec![func.clone()]);
+                    }
+
+                    after_each.push(func.clone())
+                },
+                "describe" => {
+                    let id = *func_idx as i32;
+                    suite_new.groups.get_mut(&id).unwrap().name = name.clone();
+                },
+                _ => {
+                    let id = *func_idx as i32;
+                    let parent_id = get_parent_id(id.clone(), test_groups.clone());
+                    suite_new.groups.get_mut(&parent_id).unwrap().tests.push(Test::new(name.to_string(), *should_fail, func.clone()));
+                    tests.push(Test::new(name.to_string(), *should_fail, func))
+                },
             };
         }
 
+        for (id, funcs) in before_each_new {
+            for test in suite_new.groups.get_mut(&id).unwrap().tests.iter_mut() {
+                test.before_hooks = funcs.clone();
+            }
+        }
+
+        for (id, funcs) in after_each_new {
+            for test in suite_new.groups.get_mut(&id).unwrap().tests.iter_mut() {
+                test.after_hooks = funcs.clone();
+            }
+        }
+
+        println!("{:?}", suite_new);
         suite.tests = tests
             .into_iter()
             .map(|mut test| {
@@ -187,6 +297,61 @@ impl<C: Blockchain> From<&MatchstickInstance<C>> for TestSuite {
             })
             .collect();
 
-        suite
+        suite_new
     }
+}
+
+fn get_parent_id(id: i32, test_groups: BTreeMap<i32, Vec<i32>>) -> i32 {
+    let mut parent_id = 0;
+
+    for (parent, children) in test_groups.iter() {
+        if *parent == id {
+            parent_id = id;
+            break;
+        } else {
+            if children.contains(&id) {
+                parent_id = *parent;
+                break;
+            }
+        }
+    }
+
+    parent_id
+}
+
+fn test_groups(path: &str) -> BTreeMap<i32, Vec<i32>> {
+    let mut wasm = twiggy_parser::read_and_parse(path, twiggy_traits::ParseMode::Auto).unwrap();
+
+    let mut opts = twiggy_opt::Paths::new();
+    opts.add_function("anonymous".to_string());
+    opts.set_using_regexps(true);
+    opts.set_descending(true);
+    opts.set_max_paths(0);
+    let paths = twiggy_analyze::paths(&mut wasm, &opts).unwrap();
+    let mut buf = Vec::new();
+    paths.emit_json(&mut wasm, &mut buf).unwrap();
+    let result = String::from_utf8(buf).unwrap();
+    let values: Value = serde_json::from_str(&result).unwrap();
+
+    let mut prev_parent_id = 0;
+
+    values
+        .as_array()
+        .unwrap()
+        .iter()
+        .enumerate()
+        .filter_map(|(i, obj)| {
+            let regex = Regex::new(r#"test~anonymous.{1}\d+$"#).expect("Incorrect regex");
+            let name = obj["name"].as_str().unwrap().to_string();
+            if regex.is_match(&name) {
+                let p_id = i as i32 + 1;
+                let children: Vec<i32> = (prev_parent_id+1..p_id).collect();
+                prev_parent_id = p_id;
+
+                Some((p_id, children))
+            } else {
+                None
+            }
+        }).collect()
+
 }
