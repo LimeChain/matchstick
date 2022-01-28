574348262bb1e14046fbf99a1c790d3bec0cc1fc
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-01-28 17:56:46 +0200

    Add config module

---
+++ src/config.rs
@@ -0,0 +1,53 @@
+use serde_yaml::Value;
+use std::path::PathBuf;
+
+pub struct MatchstickConfig {
+    pub libs_path: String,
+    pub tests_path: String,
+}
+
+impl Default for MatchstickConfig {
+    fn default() -> MatchstickConfig {
+        MatchstickConfig {
+            libs_path: "./node_modules".to_string(),
+            tests_path: "./tests".to_string(),
+        }
+    }
+}
+
+pub fn parse_matchstick_config() -> MatchstickConfig {
+    let mut config = MatchstickConfig::default();
+
+    if PathBuf::from("matchstick.yaml").exists() {
+        let matchstick_config = std::fs::read_to_string("matchstick.yaml").unwrap();
+        let matchstick_yaml: Value = serde_yaml::from_str(&matchstick_config)
+            .unwrap_or_else(|_| Value::String("".to_string()));
+
+        let mut tests_path = matchstick_yaml
+            .get("testsFolder")
+            .unwrap_or(&Value::String(config.tests_path))
+            .as_str()
+            .unwrap()
+            .to_string();
+
+        let mut libs_path = matchstick_yaml
+            .get("libsFolder")
+            .unwrap_or(&Value::String(config.libs_path))
+            .as_str()
+            .unwrap()
+            .to_string();
+
+        if tests_path.ends_with('/') {
+            tests_path.pop();
+        }
+
+        if libs_path.ends_with('/') {
+            libs_path.pop();
+        }
+        
+        config.tests_path = tests_path;
+        config.libs_path = libs_path;
+    }
+
+    config
+}
