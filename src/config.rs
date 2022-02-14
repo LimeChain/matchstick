e3a82344fee435be2690064875f35f609c464804
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-02-14 11:31:28 +0200

    Refactor, add documentation and tests

--- src/config.rs
+++ src/config.rs
@@ -3,27 +3,29 @@ use std::path::PathBuf;
 
 use crate::logging::Log;
 
-const CONFIG: &str = "matchstick.yaml";
-
 pub struct MatchstickConfig {
     pub libs_path: String,
     pub tests_path: String,
 }
 
 impl MatchstickConfig {
-    fn default() -> MatchstickConfig {
+    /// fn new() - > MatchstickConfig
+    /// Creates a MatchstickConfig with default values
+    fn new() -> MatchstickConfig {
         MatchstickConfig {
             libs_path: "./node_modules".to_string(),
             tests_path: "./tests".to_string(),
         }
     }
-    /// Reads and checks if libFolder and/or testsFolder are defined.
-    /// Otherwise returns the default values.
-    pub fn new() -> MatchstickConfig {
-        let mut config = MatchstickConfig::default();
 
-        if PathBuf::from(CONFIG).exists() {
-            let matchstick_yaml = parse_yaml();
+    /// pub fn from(path: &str) -> MatchstickConfig
+    /// Creates a new MatchstickConfig from the passed matchstcik config.
+    /// If the config does not exist or keys are missing, returns the default values
+    pub fn from(path: &str) -> MatchstickConfig {
+        let mut config = MatchstickConfig::new();
+
+        if PathBuf::from(path).exists() {
+            let matchstick_yaml = parse_yaml(path);
             // Tries to get the tests or libs folder value from the config file.
             // If the attribute doesn't exist returns the default value.
             config.tests_path = extract_string(&matchstick_yaml, "testsFolder", config.tests_path);
@@ -34,22 +36,42 @@ impl MatchstickConfig {
     }
 }
 
+/// fn extract_string(value: &Value, key: &str, default: String) -> String
+/// Extracts the string value of the passed key from the parsed config
+/// Fallbacks to the default value if it fails to extract the string for some reason
 fn extract_string(value: &Value, key: &str, default: String) -> String {
     value
         .get(key)
-        .unwrap_or(&Value::String(default.clone()))
+        .unwrap_or({
+            Log::Warning(format!(
+                "Failed to get `{}` from matchstick.config! Using default config value.",
+                key
+            ))
+            .println();
+            &Value::String(default.clone())
+        })
         .as_str()
-        .unwrap_or(&default)
+        .unwrap_or({
+            Log::Warning(format!(
+                "Failed to parse `{}` as str! Using default config value.",
+                key
+            ))
+            .println();
+            &default
+        })
         .to_string()
 }
 
-fn parse_yaml() -> Value {
-    let matchstick_config = std::fs::read_to_string(CONFIG).unwrap_or_else(|err| {
+/// fn parse_yaml(path: &str) -> Value
+/// Parses the matchstick.yaml file
+/// If the parsing fails returns an empty Value::String()
+fn parse_yaml(path: &str) -> Value {
+    let matchstick_config = std::fs::read_to_string(path).unwrap_or_else(|err| {
         panic!(
             "{}",
             Log::Critical(format!(
                 "Something went wrong while trying to read `{}`: {}",
-                CONFIG, err,
+                path, err,
             )),
         )
     });
@@ -58,9 +80,63 @@ fn parse_yaml() -> Value {
     serde_yaml::from_str(&matchstick_config).unwrap_or_else(|_| {
         Log::Warning(format!(
             "{} is empty or contains invalid values! Using default configuration.",
-            CONFIG
+            path
         ))
         .println();
         Value::String("".to_string())
     })
+}
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+    use crate::config::MatchstickConfig;
+    use serde_yaml::Value;
+
+    #[test]
+    #[should_panic(
+        expected = "🆘 Something went wrong while trying to read `mocks/configs/no_config.yaml`: No such file or directory (os error 2)"
+    )]
+    fn parse_yaml_should_panic_when_file_is_missing() {
+        parse_yaml("mocks/configs/no_config.yaml");
+    }
+
+    #[test]
+
+    fn parse_yaml_returns_empty_string_value_if_config_is_empty() {
+        let yaml = parse_yaml("mocks/configs/matchstick_empty.yaml");
+
+        assert_eq!(yaml, Value::String("".to_string()))
+    }
+
+    #[test]
+    fn extract_string_returns_value_as_string() {
+        let config_yaml = parse_yaml("mocks/configs/matchstick.yaml");
+        let test_folder = extract_string(&config_yaml, "testsFolder", "./tests".to_string());
+
+        assert_eq!(test_folder, "./specs".to_string())
+    }
+
+    #[test]
+    fn extract_string_returns_default_when_key_is_missing() {
+        let config_yaml = parse_yaml("mocks/configs/matchstick.yaml");
+        let test_folder = extract_string(&config_yaml, "libsFolder", "./node_modules".to_string());
+
+        assert_eq!(test_folder, "./node_modules".to_string())
+    }
+
+    #[test]
+    fn config_from_returns_default_values_if_no_config() {
+        let config = MatchstickConfig::from("mocks/configs/no_config.yaml");
+
+        assert_eq!(config.libs_path, "./node_modules".to_string());
+        assert_eq!(config.tests_path, "./tests".to_string());
+    }
+
+    #[test]
+    fn config_from_returns_custom_folder_from_config() {
+        let config = MatchstickConfig::from("mocks/configs/matchstick.yaml");
+
+        assert_eq!(config.tests_path, "./specs".to_string());
+    }
 }
--- src/subgraph.rs
+++ src/subgraph.rs
@@ -2,7 +2,39 @@ use serde_yaml::{Sequence, Value};
 use std::collections::HashMap;
 use std::fs;
 
-/// Extracts the handler name
+use crate::Log;
+
+/// fn parse_yaml(path: &str) -> Value
+/// Parses the yaml file from the passed path
+/// Will panic if the file does not exist or can't be parsed
+fn parse_yaml(path: &str) -> Value {
+    let subgraph_yaml_contents = fs::read_to_string(path).unwrap_or_else(|err| {
+        panic!(
+            "{}",
+            Log::Critical(format!(
+                "Something went wrong while trying to read `{}`: {}",
+                path, err,
+            )),
+        )
+    });
+
+    let subgraph_yaml: Value =
+        serde_yaml::from_str(&subgraph_yaml_contents).unwrap_or_else(|err| {
+            panic!(
+                "{}",
+                Log::Critical(format!(
+                    "Something went wrong when parsing `{}`: {}",
+                    path, err,
+                )),
+            )
+        });
+
+    subgraph_yaml
+}
+
+/// fn extract_string(value: &Value, key: &str) -> String
+/// Extracts the String value of the passed key
+/// Panics if the key is missing or can't convert the value to string
 fn extract_string(value: &Value, key: &str) -> String {
     value
         .get(key)
@@ -12,6 +44,10 @@ fn extract_string(value: &Value, key: &str) -> String {
         .to_string()
 }
 
+/// fn extract_vec(value &Value, key: &str) -> Sequence
+/// Extracts the value of the passed key as Sequence
+/// Will return an empty Vec if the key is missing
+/// Will panic if the value can't be parsed as Sequence
 fn extract_vec(value: &Value, key: &str) -> Sequence {
     value
         .get(key)
@@ -21,17 +57,10 @@ fn extract_vec(value: &Value, key: &str) -> Sequence {
         .to_vec()
 }
 
-fn parse_yaml() -> Value {
-    let subgraph_yaml_contents = fs::read_to_string("subgraph.yaml")
-        .expect("Something went wrong reading the 'subgraph.yaml' file");
-    let subgraph_yaml: Value = serde_yaml::from_str(&subgraph_yaml_contents)
-        .expect("Something went wrong when parsing 'subgraph.yaml'. Please ensure that the file exists and is valid.");
-
-    subgraph_yaml
-}
-
-fn parse_sources() -> Sequence {
-    let subgraph_yaml = parse_yaml();
+/// fn parse_sources(path: &str) -> Sequence
+/// Extracts the sources declared under dataSources or templates in the subraph.yaml
+fn parse_sources(path: &str) -> Sequence {
+    let subgraph_yaml = parse_yaml(path);
 
     let mut sources = vec![];
     sources.append(&mut extract_vec(&subgraph_yaml, "dataSources"));
@@ -40,8 +69,11 @@ fn parse_sources() -> Sequence {
     sources
 }
 
-pub fn collect_handlers() -> HashMap<String, Vec<String>> {
-    parse_sources()
+/// fn collect_handlers(path: &str) -> HashMap<String, Vec<String>>
+/// collects the event and call handlers for each source
+/// declared under dataSources or templates
+pub fn collect_handlers(path: &str) -> HashMap<String, Vec<String>> {
+    parse_sources(path)
         .iter()
         .map(|source| {
             let name = extract_string(source, "name");
@@ -65,12 +97,79 @@ pub fn collect_handlers() -> HashMap<String, Vec<String>> {
         .collect()
 }
 
-pub fn get_schema_location() -> String {
-    let subgraph_yaml = parse_yaml();
+/// fn get_schema_location(path: &str) -> String
+/// Extracts the schema location from subraph.yaml
+/// Will panic if the `schema` or `file` key is missing
+pub fn get_schema_location(path: &str) -> String {
+    let subgraph_yaml = parse_yaml(path);
 
     let schema = subgraph_yaml
         .get("schema")
         .expect("Couldn't get schema from yaml file.");
 
     extract_string(schema, "file")
+}
+
+#[cfg(test)]
+mod schema_tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    #[should_panic(
+        expected = "🆘 Something went wrong while trying to read `mocks/subgraphs/no_subgraph.yaml`: No such file or directory (os error 2)"
+    )]
+    fn parse_yaml_should_panic_when_file_is_missing() {
+        parse_yaml("mocks/subgraphs/no_subgraph.yaml");
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "🆘 Something went wrong when parsing `mocks/subgraphs/subgraph_invalid.yaml`: while parsing a block mapping, did not find expected key at line 6 column 1"
+    )]
+    fn parse_yaml_should_panic_when_file_is_invalid() {
+        parse_yaml("mocks/subgraphs/subgraph_invalid.yaml");
+    }
+
+    #[test]
+    fn get_schema_location_returns_schema_location() {
+        let schema_location = get_schema_location("mocks/subgraphs/subgraph.yaml");
+
+        assert_eq!(schema_location, "./schema.graphql".to_string())
+    }
+
+    #[test]
+    #[should_panic(expected = "Couldn't find key `name` in subgraph.yaml")]
+    fn parse_string_should_panic_if_key_missing() {
+        let yaml = parse_yaml("mocks/subgraphs/subgraph_no_name.yaml");
+        extract_string(&yaml, "name");
+    }
+
+    #[test]
+    fn collect_handlers_returns_all_handlers() {
+        let handlers = collect_handlers("mocks/subgraphs/subgraph.yaml");
+        let mut expected: HashMap<String, Vec<String>> = HashMap::new();
+        expected.insert(
+            "Gravity".to_string(),
+            vec![
+                "handleNewGravatar".to_string(),
+                "handleCreateGravatar".to_string(),
+            ],
+        );
+        expected.insert(
+            "GraphTokenLockWallet".to_string(),
+            vec!["handleTokensReleased".to_string()],
+        );
+
+        assert_eq!(handlers, expected)
+    }
+
+    #[test]
+    fn collect_handlers_returns_empty_vec_if_no_handlers() {
+        let handlers = collect_handlers("mocks/subgraphs/subgraph_no_handlers.yaml");
+        let mut expected: HashMap<String, Vec<String>> = HashMap::new();
+        expected.insert("Gravity".to_string(), vec![]);
+
+        assert_eq!(handlers, expected)
+    }
 }
