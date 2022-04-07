db31965de701357e22feaa7656ad7eb5626913cc
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-04-07 10:58:24 +0300

    Update tests

--- mocks/as/mock-includes.test.ts
+++ mocks/as/mock-includes.test.ts
@@ -1,7 +1,6 @@
 import { handleNewGravatars, createNewGravatarEvent, trySaveGravatarFromContract, saveGravatarFromContract } from "./utils"
 import { test, log } from 'matchstick-as/assembly/index'
-import { Gravatar } from '../generated/schema'
-import { handleCreateGravatar, handleNewGravatar } from "../../src/gravity"
+import { handleCreateGravatar, handleNewGravatar } from "../src/gravity"
 
 test("0", () => {})
 
